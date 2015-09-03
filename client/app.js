342f7a946810197f5d8022a7d15fe600eaabdfa0
ilanMa <ilan.mamontov@gmail.com>
2015-09-03 17:40:52 -0400

    adds view routing

--- client/app.js
+++ client/app.js
@@ -1,1 +1,13 @@
-var simulator = angular.module('simulator', []);
+var simulation = angular.module('simulation', ['ngRoute']);
+
+console.log("### APP JS")
+
+simulation.config(function ($routeProvider) {
+      $routeProvider
+        .when('/',{
+            templateUrl: 'views/landing.html'
+        })
+        .otherwise({
+          redirectTo: '/'
+        });
+    });
--- server.js
+++ server.js
@@ -3,12 +3,8 @@ var http = require('http');
 
 var app = express();
 
-// app.use(express.static(__dirname + '/client'));
+app.use(express.static(__dirname + '/client'));
 
 app.listen(process.env.PORT || 3000, function() {
 	console.log("Running Server...")
-});
-
-app.get('/', function(req, res) {
-    res.sendfile('client/index.html')
 });
