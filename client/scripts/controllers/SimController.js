05131cc653aa3d0d9460cd7caf253a9df18704e3
ilanMa <ilan.mamontov@gmail.com>
2016-02-06 17:10:44 -0500

    Return champ info working

--- client/scripts/controllers/SimController.js
+++ client/scripts/controllers/SimController.js
@@ -1,6 +1,20 @@
-simulation.controller('SimController', ['$scope', function() {
-    'use strict';
+simulation.controller('SimController', [
+    '$scope',
+    '$http',
+    function(
+    	$scope, 
+    	$http) {
+        'use strict';
 
-    console.log('### Sim Controller');
+        console.log('### Sim Controller');
 
-}])
+        $scope.getSkins = function() {
+            $http.get('/skins').then(function(res) {
+                console.log(res);
+            }, function(err) {
+                console.log(err)
+            })
+        }
+        $scope.getSkins();
+    }
+])
--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 var express = require('express');
 var http = require('http');
+var request = require('request');
 
 var app = express();
 
 app.use(express.static(__dirname + '/client'));
 
 app.listen(process.env.PORT || 3000, function() {
-	console.log("Running Server...")
+    console.log("Running Server...")
 });
 
-var request = require('request');
+app.get('/skins', function(req, res) {
+    request('https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion?champData=skins&api_key=cd1e82c2-e90e-409b-8522-bb2a77a42c7d', function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.send(body);
+        } else {
+            console.log(error);
+        }
+    })
 
-request('https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion?champData=skins&api_key=cd1e82c2-e90e-409b-8522-bb2a77a42c7d', function(error,response,body) {
-	if (!error && response.statusCode == 200) {
-		console.log(body);
-	} else {
-		console.log(error);
-	}
 })
