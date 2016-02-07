bf47e6fdcf98f18e815ee5686039aab5161a870d
ilanMa <ilan.mamontov@gmail.com>
2016-02-06 22:48:48 -0500

    SimController refactor

--- client/scripts/controllers/SimController.js
+++ client/scripts/controllers/SimController.js
@@ -1,49 +1,19 @@
 simulation.controller('SimController', [
     '$scope',
     '$http',
+    'SkinService',
     function(
         $scope,
-        $http) {
+        $http,
+        SkinService) {
         'use strict';
 
-        console.log('### Sim Controller');
+        console.log('### SIM CONTROLLER');
 
         var skins = [];
+        SkinService.getChampData(function(res){
+            skins = SkinService.getSkinsData(res);
+        });
 
-        var getSkins = function() {
-            $http.get('/skins').then(function(res) {
-                storeSkins(res);
-            }, function(err) {
-                console.log(err)
-            })
-        }
-        //getSkins();
-
-        function storeSkins(res) {
-        	var data = res.data.data
-            for (var index in data) {
-            	var length = data[index].skins.length;
-                for (var i = 0; i < length; i++) {
-
-                    var tempSkin = {
-                        "name": data[index].skins[i].name,
-                        "champ": data[index].key,
-                        "num": data[index].skins[i].num
-                    };
-
-
-                    if (name === "default") {
-                        tempSkin.img = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + tempSkin.champ + "_0.jpg";
-                    } else {
-                        tempSkin.img = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + tempSkin.champ + "_" + tempSkin.num + ".jpg";
-                    }
-
-                    skins.push(tempSkin);
-                }
-
-            }
-            console.log(skins)
-
-        }
-    }
+    }   
 ])
