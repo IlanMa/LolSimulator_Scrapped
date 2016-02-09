045ec44c0b9e318928db67357e76fd5c0e097d4d
ilanMa <ilan.mamontov@gmail.com>
2016-02-09 17:12:54 -0500

    Major controller refactor

--- client/scripts/controllers/SimController.js
+++ client/scripts/controllers/SimController.js
@@ -20,44 +20,16 @@ simulation.controller('SimController', [
         //     console.log(skins);
         // });
 
-        // Initial image
-        //$scope.image = 'http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Aatrox_0.jpg';
+        // Contains skin history
+        $scope.skinHistory = GiftService.skinHistory;
 
-        // Chests opened
-        $scope.chestsOpened = 0;
-        // Dollars spent
-        $scope.dollarSpent = 0;
-        // RP spent
-        $scope.rpSpent = 0;
-        // RP gained
-        $scope.rpGained = 0;
-        // Dollar return
-        $scope.dollarReturn = 0;
-
-        // Contains all skins
-        var skins = ChampionInfo;
+        // Contains all statistics
+        $scope.statistics = GiftService.statistics;
 
         // Invoked when button is clicked
         $scope.openSkin = function() {
-            var skinResult;
-            do {
-                skinResult = skins[Math.floor(Math.random() * skins.length)];
-            } while (skinResult.name === 'default')
-            GiftService.skinHistory.push({
-                name: skinResult.name,
-                price: skinResult.price
-            })
-            $scope.skinHistory = GiftService.skinHistory;
-            // $scope.image = skinResult.img;
-            $scope.calculatePricing(skinResult);
-        }
-
-        $scope.calculatePricing = function(skin) {
-            $scope.chestsOpened += 1;
-            $scope.dollarSpent = $scope.chestsOpened * 4.25;
-            $scope.rpSpent = $scope.chestsOpened * 490;
-            $scope.rpGained += skin.price;
-            $scope.dollarReturn = ($scope.rpGained - $scope.rpSpent) * 0.0096153846153846;
+            GiftService.openSkin(); 
+            $scope.skinImage = GiftService.skinImage;
         }
 
     }
