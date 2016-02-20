b46e6949c326b182f899d29470dfd10222d921f2
ilanMa <ilan.mamontov@gmail.com>
2016-02-20 15:05:44 -0500

    Add gift animation directive

---
+++ client/scripts/directives/giftAnimation.js
@@ -0,0 +1,14 @@
+simulation.directive('giftAnimation', [
+    function() {
+        'use strict';
+
+        console.log("### GIFT ANIMATION DIRECTIVE");
+
+        return {
+            restrict: 'E',
+            link: function(scope, element, attrs) {
+
+            }
+        }
+    }
+])
