e711c2e1928091a04de46289c4042d8a24991ce0
ilanMa <ilan.mamontov@gmail.com>
2016-02-20 19:32:09 -0500

    Minor giftAnimation directive refactor

--- client/scripts/directives/giftAnimation.js
+++ client/scripts/directives/giftAnimation.js
@@ -10,29 +10,28 @@ simulation.directive('giftAnimation', [
             templateUrl: '../../views/animation.html',
             link: function(scope, element, attrs) {
                 var skinArray = [];
+                var skinsToGenerate = 40;
                 // Invoked when gift button is clicked
                 function openGift() {
                     var skins = GiftService.skinList.normal;
                     var currentLeft = 0;
                     var skinList = '';
                     if (skinArray.length) {
+                    	skinsToGenerate = 35;
                         $('.skin-list').css('left', '0px');
                         skinArray.splice(0, 35);
                         for (var i = 0; i < skinArray.length; i++) {
                             skinList += '<li>' + skinArray[i].name + '</li>';
                         }
                     }
-                    for (var i = 0; i < 40; i++) {
+                    for (var i = 0; i < skinsToGenerate; i++) {
                         var skinResult = skins[Math.floor(Math.random() * skins.length)];
                         skinArray.push(skinResult);
                         skinList += '<li>' + skinResult.name + '</li>';
                     }
-                    $('.skin-list').css('width', skinArray.length * 80);
-                    $('.skin-list').html(skinList);
-                    $('.skin-list').animate({
+                    $('.skin-list').css('width', skinArray.length * 80).html(skinList).animate({
                         left: (currentLeft = currentLeft - 2800) + 'px'
-                    }, 3000);
-                    spins += 1;
+                    }, 3000);;
                 }
 
                 $('.chest-button').on('click', openGift);
