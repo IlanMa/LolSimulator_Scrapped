61aeafd298c379c917b3e4fdad7d4d7f95799344
ilanMa <ilan.mamontov@gmail.com>
2016-02-20 19:26:50 -0500

    Animation almost complete

--- client/scripts/directives/giftAnimation.js
+++ client/scripts/directives/giftAnimation.js
@@ -1,5 +1,5 @@
 simulation.directive('giftAnimation', [
-	'GiftService',
+    'GiftService',
     function(GiftService) {
         'use strict';
 
@@ -9,22 +9,33 @@ simulation.directive('giftAnimation', [
             restrict: 'E',
             templateUrl: '../../views/animation.html',
             link: function(scope, element, attrs) {
-            	// Invoked when gift button is clicked
-           		function openGift() {
-           			var skins = GiftService.skinList.normal;
-           			var skinArray = [];
-           			var skinList = '<ul>';
-           			for (var i = 0; i < 40; i++) {
-           				var skinResult = skins[Math.floor(Math.random() * skins.length)];
-           				skinArray.push(skinResult);
-           				skinList += '<li>'+skinResult.name+'</li>';
-           			}
-           			skinList += '</ul>';
-           			$('.scroll-result').append(skinList);
-           			console.log(skinArray);
-           		}
+                var skinArray = [];
+                // Invoked when gift button is clicked
+                function openGift() {
+                    var skins = GiftService.skinList.normal;
+                    var currentLeft = 0;
+                    var skinList = '';
+                    if (skinArray.length) {
+                        $('.skin-list').css('left', '0px');
+                        skinArray.splice(0, 35);
+                        for (var i = 0; i < skinArray.length; i++) {
+                            skinList += '<li>' + skinArray[i].name + '</li>';
+                        }
+                    }
+                    for (var i = 0; i < 40; i++) {
+                        var skinResult = skins[Math.floor(Math.random() * skins.length)];
+                        skinArray.push(skinResult);
+                        skinList += '<li>' + skinResult.name + '</li>';
+                    }
+                    $('.skin-list').css('width', skinArray.length * 80);
+                    $('.skin-list').html(skinList);
+                    $('.skin-list').animate({
+                        left: (currentLeft = currentLeft - 2800) + 'px'
+                    }, 3000);
+                    spins += 1;
+                }
 
-            	$('.chest-button').on('click', openGift);
+                $('.chest-button').on('click', openGift);
             }
         }
     }
